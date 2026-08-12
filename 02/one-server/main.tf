21fccff495e3e8b3e7bd4585675dd8af4d82a5e3
kshi1313 <kshi1313@gmail.com>
2026-08-12 18:47:55 +0900

    Create Server Example

--- 02/one-server/main.tf
+++ 02/one-server/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-048f644e868baa0e8"  # us-east-2 리전에 존재하는 Amazon2023 AMI ID
+  ami           = "ami-048f644e868baa0e8" # us-east-2 리전에 존재하는 Amazon2023 AMI ID
   instance_type = "t3.micro"
   tags = {
     Name = "terraform-example"
